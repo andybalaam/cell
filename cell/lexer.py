3c3f0254ce29e0d7b473b992c0a28fd4c56ea3de
Andy Balaam <andybalaam@artificialworlds.net>
2016-06-07 01:34:07 +0100

    Move constants out of lex

--- cell/lexer.py
+++ cell/lexer.py
@@ -38,10 +38,10 @@ class PeekableStream:
         self._fill()
         return ret
 
+_midsym = "[_a-zA-Z0-9]"
+_stsym = "[_a-zA-Z]"
 
 def lex(chars_iter):
-    midsym = "[_a-zA-Z0-9]"
-    stsym = "[_a-zA-Z]"
     chars = PeekableStream(chars_iter)
     while chars.next is not None:
         c = chars.move_next()
@@ -50,7 +50,7 @@ def lex(chars_iter):
         elif c in "+-*/":           yield ("arithmetic", c)
         elif c in ("'", '"'):       yield ("string", _scan_string(c, chars))
         elif re.match("[.0-9]", c): yield ("number", _scan(c, chars, "[.0-9]"))
-        elif re.match(stsym, c):    yield ("symbol", _scan(c, chars, midsym))
+        elif re.match(_stsym, c):   yield ("symbol", _scan(c, chars, _midsym))
         elif c == "\t":
             raise Exception("Tab characters are not allowed in Cell")
         else:
