657fe332f780bf392e6c6e8e7a9e65369af3122e
Andy Balaam <andybalaam@artificialworlds.net>
2016-06-14 22:56:30 +0100

    Share code between all_examples versions

--- tests/util/all_examples.py
+++ tests/util/all_examples.py
@@ -1,17 +1,13 @@
 import os
 
 
-def all_examples():
+def all_examples(file_ext=".cell"):
     return (
         "examples/" + filename
         for filename in os.listdir("examples")
-        if filename.endswith(".cell")
+        if filename.endswith(file_ext)
     )
 
 
 def all_sessions():
-    return (
-        "examples/" + filename
-        for filename in os.listdir("examples")
-        if filename.endswith(".cellsession")
-    )
+    return all_examples(".cellsession")
