f61c2ebfe06bc91e73975d5a476bbe064b84652b
Andy Balaam <andybalaam@artificialworlds.net>
2016-06-15 02:43:18 +0100

    Allow running a .cell file

---
+++ pycell/run.py
@@ -0,0 +1,11 @@
+from pycell.chars_in_file import chars_in_file
+from pycell.env import Env
+from pycell.eval_ import eval_list
+from pycell.lexer import lex
+from pycell.parser import parse
+
+
+def run(argv, stdin, stdout, stderr):
+    env = Env()
+    with open(argv[1]) as f:
+        eval_list(parse(lex(chars_in_file(f))), env)
